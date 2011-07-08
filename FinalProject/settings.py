2f7c9425af42dca4a86d6d27d1749478c0644a8b
Kaf <a.kafu@yahoo.com>
2011-07-08 14:28:21 +0000

    edited settings.py

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -103,7 +103,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'FinalProject.urls'
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'templates')
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
