1b670922332f3bf46e3d9a2118c69c4bfe275f99
ponzie <kopponzie@yahoo.com>
2011-07-08 12:18:17 +0000

    models.py

--- FinalProject/MyFinalProject/models.py
+++ FinalProject/MyFinalProject/models.py
@@ -1,18 +1,72 @@
 
+from django.db import models
+from django.contrib import admin
 
 class Company(models.Model):
-	Name=models.CharField(max_length=60)
-	Location=models.charField(max_length=100)
-	PhoneNumber=models.IntegerField()
-
+	name=models.CharField(max_length=60)
+	location=models.charField(max_length=100)
+	phoneNumber=models.IntegerField()
+	def __unicode__(self):
+		return self.name)+","+self.location
+class MenuItem(models.Model):
+	number=models.Integer(max_length=10)
+	price=models.FloatField(max_digits=10, decimal_places=2)
+	item=models.CharField(max_length=20)
+	companyId=models.ForeignKey(Company)
+	def __unicode__(self):
+		return str(self.item)+","+str(self.price)
 
 class Customer(models.Model):
-	Phone Number=models.IntergerField()
+	phonenumber=models.IntergerField()
+	def __unicode__(self):
+		return str(self.phonenumber)
+class Payment(models.Model):
+	amount=models.FloatField(max_digits=10, decimal_places=3)
+	confirmationMessage=models.CharField(max_length=60)
+	date=models.DateTimeField(auto_now=True)
+	def __unicode__(self):
+		return str(self.amount)+","+str(self.date)
 
 class Order(models.Model):
-	Quantity=models.IntegerField()
-	Item=models.CharField(max_length=20)
-        OrderId=models.ForeignKey(Customer) 
+	quantity=models.IntegerField()
+	item=models.CharField(max_length=20)
+        orderId=models.ForeignKey(Customer) 
+	def __unicode__(self):
+		return str(self.quantity)+","+self.item
+class History(models.Model):
+	OrderId =models.ForeignKey(Order)
+	CustomerId=models.ForeignKey(Customer)
+	total=models.FloatField(max_digits=10, decimal_places=2)
+	def __unicode__(self):
+		return str(self.total)
 
-class CommentInline(admin.TabularInline):
-	model = Comment
+class MenuItemInline(admin.TabularInline):
+	model = MenuItem
+class OrderInline(admin.TabularInline):
+	model = Order
+class HistoryInline(admin.TabularInline):
+	model=History
+
+
+
+class CompanyAdmin(admin.ModelAdmin):
+	list_display = ('name','location','phoneNumber')
+	search_fields = ('name',)
+	list_filter = ('date',)
+	inlines = [MenuItemInline]
+
+class MenuItemAdmin(admin.ModelAdmin):
+	list_display=('number','price','item','companyId')
+	list_filter = ('number','price')
+
+class CustomerAdmin(admin.ModelAdmin):
+	list_display('phonenumber')
+	inlines = [OrderInline]
+	inlines = [HistoryInline]
+	
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(MenuItem,MenuItemAdmin)
+admin.site.register(Customer,CustomerAdmin)
+
+
+
