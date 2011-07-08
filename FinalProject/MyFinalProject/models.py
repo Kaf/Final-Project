a4dfdb86229636703666f7aeab8c68f6b453ccc5
Kaf <a.kafu@yahoo.com>
2011-07-08 10:41:38 +0000

    edited part of models.py

--- FinalProject/MyFinalProject/models.py
+++ FinalProject/MyFinalProject/models.py
@@ -1,3 +1,15 @@
-from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+	Name=models.CharField(max_length=60)
+	Location=models.charField(max_length=100)
+	PhoneNumber=models.IntegerField()
+
+
+class Customer(models.Model):
+	Phone Number=models.IntergerField()
+
+class Order(models.Model):
+	Quantity=models.IntegerField()
+	Item=models.CharField(max_length=20)
+        OrderId=models.IntegerField() 
