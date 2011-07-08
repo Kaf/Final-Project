7f4b34b55bc3d395c52d27870864385e645698c7
Kaf <a.kafu@yahoo.com>
2011-07-08 13:42:52 +0000

    edited views.py

--- FinalProject/MyFinalProject/views.py
+++ FinalProject/MyFinalProject/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.forms import ModelForm
+from django.http import HttpResponse
+from models import *
+
+
+def home(request):
+    return HttpResponse('Welcome To NextGen')
