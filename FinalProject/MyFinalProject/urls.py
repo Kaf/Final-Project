1ebd8ade0529bdbf0997baa5d0ac6138ac02572c
Kaf <a.kafu@yahoo.com>
2011-07-08 14:11:33 +0000

    edited urls.py

--- FinalProject/MyFinalProject/urls.py
+++ FinalProject/MyFinalProject/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
 url(r'^$', 'MyfinalProject.views.home'),
-url(r'^$', 'MyfinalProject.views.blog_list'),
-url(r'^$', 'MyfinalProject.views.blog_detail'),
-url(r'^$', 'MyfinalProject.views.blog_search'),
-url(r'^$', 'MyfinalProject.views.'),
-url(r'^$', 'MyfinalProject.views.'),
+#url(r'^$', 'MyfinalProject.views.'),
+#url(r'^$', 'MyfinalProject.views.'),
+#url(r'^$', 'MyfinalProject.views.'),
+#url(r'^$', 'MyfinalProject.views.'),
+#url(r'^$', 'MyfinalProject.views.'),
 )
 
