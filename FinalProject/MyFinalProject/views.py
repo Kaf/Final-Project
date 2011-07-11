1b4293588680062836d0e89dd242bd252e9a6011
ponzie <kopponzie@yahoo.com>
2011-07-11 10:46:16 +0000

    views.py

--- FinalProject/MyFinalProject/views.py
+++ FinalProject/MyFinalProject/views.py
@@ -18,7 +18,7 @@ def home(request):
 	return HttpResponse(t.render(c))
 def menuView(request, id):
 	com=Company.objects.get(pk=id)
-	menuItems = com.MenuItem_set.all()
+	menuItems = com.menuitem_set.all()
 	t = loader.get_template('MyFinalProject/menulist.html')
 	c = Context({'Company':Company,'com':com, 'MenuItems':MenuItems})
 	return HttpResponse(t.render(c))
