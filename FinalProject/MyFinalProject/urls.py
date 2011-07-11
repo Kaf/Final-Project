23996542a2a9715c9a7df297f91633e264c76b1b
Kaf <a.kafu@yahoo.com>
2011-07-11 10:36:56 +0000

    urls.py

--- FinalProject/MyFinalProject/urls.py
+++ FinalProject/MyFinalProject/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
 url(r'^home$', 'MyFinalProject.views.home'),
 url(r'^company$', 'MyFinalProject.views.listView'),
-url(r'^menu/(?P<id>\d+)$', 'MyFinalProject.views.menuView'),
-#url(r'^$', 'MyFinalProject.views.'),
+url(r'^menulist/(?P<id>\d+)$', 'MyFinalProject.views.menuView'),
+url(r'^menulist$', 'MyFinalProject.views.menuView'),
 #url(r'^$', 'MyFinalProject.views.'),
 #url(r'^$', 'MyFinalProject.views.'),
 )
