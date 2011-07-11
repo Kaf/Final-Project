8e085af61d46aee6f28d1bdba66b3034e74e09f6
ponzie <kopponzie@yahoo.com>
2011-07-11 09:53:20 +0000

    just edited urls.py

--- FinalProject/MyFinalProject/urls.py
+++ FinalProject/MyFinalProject/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
 url(r'^home$', 'MyFinalProject.views.home'),
-url(r'^company$', 'MyFinalProject.views.listView'),
+url(r'^company/$', 'MyFinalProject.views.listView'),
 url(r'^menu/(?P<id>\d+)$', 'MyFinalProject.views.menuView'),
 #url(r'^$', 'MyFinalProject.views.'),
 #url(r'^$', 'MyFinalProject.views.'),
